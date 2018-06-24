f94f0ce60014ea856bea0671691852508c3568aa
York Wu <yorkwuo@gmail.com>
2018-06-24 08:22:30 +0800

    new

---
+++ dining/.godel/ghtm.tcl
@@ -0,0 +1,140 @@
+ghtm_top_bar
+#ghtm_list_files *
+
+ghtm_filter_notes
+gnotes {
+*#. taipei 101 
+Lawry's勞瑞斯牛肋排餐廳
+}
+gnotes {
+*#. taipei 101 A4
+香茅廚泰式餐廳新光A4店
+}
+
+gnotes {
+*#. taipei 101
+鼎泰豐
+}
+
+gnotes {
+*#. taipei 吴兴街
+大王麻辣乾麵 KingSpicy
+}
+
+gnotes {
+*#. taipei 延吉街
+麻辣壹號店
+}
+
+gnotes {
+*#. taipei 延吉街
+醉麻辣
+}
+
+gnotes {
+*#. taipei
+French Windows 琺蘭綺瑥朵茶餐館
+}
+
+gnotes {
+*#. taipei 松烟
+11 Cafe' 早午餐 & 創意義大利麵專賣店
+}
+
+gnotes {
+*#. taipei 松烟
+AWESOME BURGER
+}
+
+gnotes {
+*#. taipei
+KONGBUL
+}
+
+gnotes {
+*#. taipei
+胡饕米粉湯•黑白切
+}
+
+
+gnotes {
+*#. taipei 光復南路
+The Shrimp Daddy 蝦老爹美食海鮮
+}
+
+gnotes {
+*#. taipei 光復南路
+溫德德式烘焙餐館(大安店)
+}
+
+gnotes {
+*#. taipei 光復南路
+起家雞韓式炸雞 光復店
+}
+
+gnotes {
+*#. taipei 市民
+布娜飛比利時啤酒餐廳 Bravo Beer 台北市民店
+}
+
+
+gnotes {
+*#. taipei 延吉街
+大師兄銷魂麵舖-忠孝店
+}
+
+
+gnotes {
+*#. taipei
+小滿苑燒肉 KOMITSU yakiniku
+延吉街
+}
+
+gnotes {
+*#. taipei
+Dancing Pig 豬跳舞小餐館
+}
+
+ghtm_notes {
+*#. taipei 
+先進海產店
+}
+ghtm_notes {
+*#. taipei 鹅肉 中山区 台北 吉林路
+阿城鵝肉
+}
+ghtm_notes {
+*#. 新竹 hsinchu night barbecue
+私嚐壹
+私嚐貳
+}
+gnotes {
+*#. 新竹 hsinchu noon japanese
+鼎泰豐
+}
+gnotes {
+*#. 新竹 hsinchu noon japanese
+SPASSO
+}
+gnotes {
+*#. 新竹 hsinchu noon japanese
+禧樂 丼飯 刺身 炭燒
+}
+gnotes {
+*#. 新竹 hsinchu korean
+小蒙牛頂級麻辣養生鍋-新竹晶品城店 ×××××
+}
+gnotes {
+*#. 新竹 hsinchu
+阿華什錦麵
+}
+gnotes {
+*#. 新竹 hsinchu
+YATS葉子 Stay to eat
+}
+
+ghtm_notes {
+*#. taipei korean
+2018/6/17 Sun.
+Broccoli beer韓國餐酒食堂, 我们四
+}
